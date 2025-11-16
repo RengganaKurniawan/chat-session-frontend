7cba822452eb5d4c0068114fab747d71edd6deec
Jason Agung <jason.kelvin.agung@gmail.com>
2025-11-16 18:25:23 +0700

    tailwind setup and test

--- src/page/MainPage.tsx
+++ src/page/MainPage.tsx
@@ -1,9 +1,11 @@
 function MainPage() {
-    return(
-        <div>
-            <h1>Main</h1>
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-blue-100">
+            <h1 className="text-3xl font-bold text-blue-600">
+                Tailwind jalan akhirnya!
+            </h1>
         </div>
-    )
+    );
 }
 
 export default MainPage;
