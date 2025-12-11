d872adbeb314fa59b33513612f2b87502cda5b6a
RengganaKurniawan <6182201071@student.unpar.ac.id>
2025-12-11 11:46:45 +0700

    add protectedRoute

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,33 @@ import { Routes, Route } from "react-router-dom";
 import LoginPage from "./page/LoginPage";
 import MainPage from "./page/MainPage";
 import SessionsSplitView from "./page/SessionsSplitView";
+import ProtectedRoute from "./component/ProtectedRoute";
 
 function App(){
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/" element={<MainPage />} />
-
-      <Route path="/sessions" element={<SessionsSplitView />} />
+      <Route 
+        path="/login" 
+        element={
+          <LoginPage />
+        } 
+      />
+      <Route 
+        path="/" 
+        element={
+          <ProtectedRoute>
+            <MainPage />
+          </ProtectedRoute>
+        } 
+      />
+      <Route 
+        path="/sessions" 
+        element={
+          <ProtectedRoute>
+            <SessionsSplitView />
+          </ProtectedRoute>
+          } 
+        />
     </Routes>
   )
 }
